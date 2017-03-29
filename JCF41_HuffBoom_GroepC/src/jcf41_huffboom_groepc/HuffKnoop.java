e48d71a7b9ba449b42c752a758ee0f3f8f84f4d4
Sjobus <sibevanetten@hotmail.com>
2017-03-29 13:53:52 +0200

    frequentie

---
+++ JCF41_HuffBoom_GroepC/src/jcf41_huffboom_groepc/HuffKnoop.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcf41_huffboom_groepc;
+
+/**
+ *
+ * @author Sibe
+ */
+public class HuffKnoop implements Comparable<HuffKnoop> {
+    public char character;
+    public Integer freq;
+    public HuffKnoop leftChild, rightChild;
+    
+    public HuffKnoop(char character, int freq)
+    {
+        this.character = character;
+        this.freq = freq;
+    }
+    
+    @Override
+    public int compareTo(HuffKnoop other) {
+        return Integer.compare(this.freq, other.freq);
+    }
+    @Override
+    public String toString(){
+        return character + ": " + freq;
+    }
+}
--- JCF41_HuffBoom_GroepC/src/jcf41_huffboom_groepc/JCF41_HuffBoom_GroepC.java
+++ JCF41_HuffBoom_GroepC/src/jcf41_huffboom_groepc/JCF41_HuffBoom_GroepC.java
@@ -5,6 +5,8 @@
  */
 package jcf41_huffboom_groepc;
 
+import java.util.*;
+
 /**
  *
  * @author Sibe
@@ -15,7 +17,59 @@ public class JCF41_HuffBoom_GroepC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String zin = "Hello world";
+        char[] chars = zin.toCharArray();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        PriorityQueue<HuffKnoop> knoopList = new PriorityQueue<>();
+        freq = Frequentie(chars);
+        knoopList = MaakKnoop(freq);
+        System.out.println(freq);
+        System.out.println(knoopList.toString());
     }
+    
+    /**
+     * Kijkt hoe vaak een char voor komt in de char[].
+     * @param karacters
+     * @return HashMap<Character,Integer>
+     */
+    public static HashMap Frequentie(char[] karacters)
+    {
+        HashMap<Character, Integer> frequentie = new HashMap<>();
+        for(char c : karacters)
+        {
+            Integer n = frequentie.get(c);
+            if(n == null)
+            {
+                n = 1;
+            }
+            else
+            {
+                n++;
+            }
+            frequentie.put(c,n);
+        }
+        
+        
+        return frequentie;
+    }
+    
+    /**
+     * 
+     * @param freq
+     * @return List<HuffKnoop>
+     */
+    public static PriorityQueue<HuffKnoop>MaakKnoop(HashMap<Character, Integer> freq)
+    {
+        //List<HuffKnoop> knopen = new ArrayList<>();
+        PriorityQueue pq = new PriorityQueue<HuffKnoop>();
+        for(char c : freq.keySet())
+        {
+            HuffKnoop knoop = new HuffKnoop(c,freq.get(c));
+            pq.add(knoop);
+        }
+        
+        return pq;
+    }
+    
     
 }
